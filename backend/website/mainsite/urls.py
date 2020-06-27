1f199dc6284126ebab8bfa7b0dd2588cc855a8bd
Roshan Bangar <roshanbangar21@gmail.com>
2020-06-27 14:55:56 +0530

    navbar linking working and done

--- backend/website/mainsite/urls.py
+++ backend/website/mainsite/urls.py
@@ -4,5 +4,16 @@ from .import views
 urlpatterns = [
 	path('', views.index, name = 'mainsite'),
 	path('geninfo/', views.geninfo, name = 'geninfo'),
+	path('sctu/', views.sctu, name = 'sctu'),
+	path('infra/', views.infra, name = 'infra'),
+	path('prmsg/', views.prmsg, name = 'prmsg'),
+	path('procedure/', views.procedure, name = 'procedure'),
+	path('age/', views.age, name = 'age'),
+	path('academic/', views.academic, name = 'academic'),
+	path('others/', views.others, name = 'others'),
+	path('lib/', views.lib, name = 'lib'),
+	path('scilab/', views.scilab, name = 'scilab'),
+	path('complab/', views.complab, name = 'complab'),
+	path('hostel/', views.hostel, name = 'hostel'),
 	
 ]
--- backend/website/mainsite/views.py
+++ backend/website/mainsite/views.py
@@ -11,3 +11,40 @@ def geninfo(request):
 	return render(request, 'mainsite/geninfo.html')
 	# return HttpResponse('Hello')')
 	# return HttpResponse('Hello')
+
+
+def sctu(request):
+	return render(request, 'mainsite/sctu.html')
+	
+def infra(request):
+	return render(request, 'mainsite/infra.html')
+
+def prmsg(request):
+	return render(request, 'mainsite/prmsg.html')
+
+
+
+def procedure(request):
+	return render(request, 'mainsite/procedure.html')
+
+def age(request):
+	return render(request, 'mainsite/age.html')
+
+def academic(request):
+	return render(request, 'mainsite/academic.html')
+
+def others(request):
+	return render(request, 'mainsite/others.html')
+	
+def lib(request):
+	return render(request, 'mainsite/lib.html')
+	
+def scilab(request):
+	return render(request, 'mainsite/scilab.html')
+	
+def complab(request):
+	return render(request, 'mainsite/complab.html')
+	
+def hostel(request):
+	return render(request, 'mainsite/hostel.html')
+	
