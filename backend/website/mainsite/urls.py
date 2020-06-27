e6af5415a8aafb9e9c0d07293b5144bde8ca679b
Roshan Bangar <roshanbangar21@gmail.com>
2020-06-27 14:36:18 +0530

    editing

--- backend/website/mainsite/urls.py
+++ backend/website/mainsite/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
 	path('', views.index, name = 'mainsite'),
+	path('geninfo/', views.geninfo, name = 'geninfo'),
 	
 ]
--- backend/website/mainsite/views.py
+++ backend/website/mainsite/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render, HttpResponse
 def index(request):
 	return render(request, 'mainsite/index.html')
 	# return HttpResponse('Hello')
+
+
+def geninfo(request):
+	# return render(request, 'def index(request):
+	return render(request, 'mainsite/geninfo.html')
+	# return HttpResponse('Hello')')
+	# return HttpResponse('Hello')
